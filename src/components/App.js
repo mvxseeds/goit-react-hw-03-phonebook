eccc442332a2d37b3b11a73ee93de7d388769bf6
Max Semena <me.semena@gmail.com>
2023-06-01 16:16:27 +0300

    Added localStorage contacts saving and preloading

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { ModernNormalize } from "emotion-modern-normalize";
 import { GlobalStyle } from "GlobalStyles";
 import { nanoid } from "nanoid";
+import contacts from "../contacts.json";
 
 import ContactForm from "./ContactForm";
 import Filter from "./Filter";
@@ -10,14 +11,27 @@ import ContactList from "./ContactList";
 export class App extends Component {
   state = {
     // hardcoded data from task for testing
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+  
+  
+  componentDidMount() {
+	const contacts = localStorage.getItem('contacts');
+	const parsedContacts = JSON.parse(contacts);
+	
+	if (parsedContacts) {
+		this.setState({ contacts: parsedContacts });
+	}
+	
+  };
+  
+  componentDidUpdate(prevProps, prevState) {
+	if (this.state.contacts !== prevState.contacts) {
+	  localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+	}
+  };
+
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
@@ -46,6 +60,7 @@ export class App extends Component {
       ),
     }));
   };
+  
 
   render() {
     const { contacts, filter } = this.state;
